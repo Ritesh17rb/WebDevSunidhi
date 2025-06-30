9a8853982ecf31a07f8b5ad69e66b28a57db5c17
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2025-06-30 21:59:45 +0530

    Experiment

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Counter from './components/Counter/Counter';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 import Services from './components/pages/Services';
 import News from './components/pages/News/News';
+import DynamicGradient from './components/pages/Gradient/DynamicGradient';
 
 function App() {
 
@@ -16,11 +16,12 @@ function App() {
       <Header />
       <div className="app-content">
         <Routes>
-          <Route path="/" element={<Counter />} />
+          <Route path="/" element={<DynamicGradient />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/news" element={<News />} />
+          <Route path="/dynamic-bg" element={<DynamicGradient />} />
         </Routes>
       </div>
       <Footer />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import './Header.css';
-import { Link } from 'react-router-dom';
+  import React from 'react';
+  import './Header.css';
+  import { Link } from 'react-router-dom';
 
-function Header() {
-  return (
-    <header className="header">
-      <div className="logo">⚛️ MyApp</div>
-      <nav className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About Us</Link>
-        <Link to="/services">Services</Link> 
-        <Link to="/contact">Contact</Link> 
-         <Link to="/news">News</Link>
-      </nav>
-    </header>
-  );
-}
+  function Header() {
+    return (
+      <header className="header">
+        <div className="logo">⚛️ MyApp</div>
+        <nav className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">Counter</Link>
+          <Link to="/services">Services</Link> 
+          <Link to="/contact">Contact</Link> 
+          <Link to="/news">News</Link>
+          <Link to="/dynamic-bg">See Magic⭐</Link>
+        </nav>
+      </header>
+    );
+  }
 
-export default Header;
+  export default Header;
